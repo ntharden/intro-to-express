93e8981475f146aaa1d6f7c3fe3f955e56976240
nathanth <nathan.hardenburg@icloud.com>
2022-06-13 22:06:12 -0500

    initial commit

---
+++ data/student-data.js
@@ -0,0 +1,9 @@
+const students = [
+  first: 'Gurleen', last: 'Mcguire', _id: 125223
+  first: 'Kyron', last: 'Phelps', _id: 127904
+  first: 'Elle-May', last: 'Irwin', _id: 139608
+]
+
+export {
+  students
+}
